e3c7baf944b82842182b51e03143346f9257275d
koheiM <matsudakohei88@gmail.com>
2020-11-27 13:47:55 +0900

    Make bottom bunner

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Footer from '../components/footer.js'
 
 export default function Home() {
   return (
@@ -50,16 +51,7 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <Footer className={styles.footer} />
     </div>
   )
 }
