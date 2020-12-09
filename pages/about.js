0caa4a2b9337eb93f61971a6710c5036a89cd47a
koheiM <matsudakohei88@gmail.com>
2020-12-09 18:51:51 +0900

    erase header again

--- pages/about.js
+++ pages/about.js
@@ -4,12 +4,10 @@ import Layout from '../components/layout'
 
 export default function about() {
   return (
-    <>
-      <Layout>
-        <HeaderBanner banner="B" />
-        <HeaderMenu aboutMeHover="" aboutMeActive="text-gray-100 bg-gray-900" />
+    <Layout>
+      {/* <HeaderBanner banner="B" /> */}
+      <HeaderMenu aboutMeHover="" aboutMeActive="text-gray-100 bg-gray-900" />
 
-      </Layout>
-    </>
+    </Layout>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,41 @@
 import Layout from '../components/layout'
-import HeaderBanner from '../components/headerBanner'
+// import HeaderBanner from '../components/headerBanner'
 import HeaderMenu from '../components/headerMenu'
 
 export default function Home() {
   return (
-    <>
-      <Layout>
-
-        <HeaderBanner banner="A" />
-        <HeaderMenu homeHover="" homeActive="text-gray-100 bg-gray-900" />
-
-        
-
-          <div className="mx-2 px-2 max-w-screen-sm">
-            <p className='my-1 py-1 text-3xl text-gray-800'>Recent News</p>
+    <Layout>
+      {/* <HeaderBanner banner="A" /> */}
+      <HeaderMenu homeHover="" homeActive="text-gray-100 bg-gray-900" />
 
 
-            <article>
-              <section>
-                {/* <span>Heading </span>
+
+      <div className="mx-2 px-2 max-w-screen-sm">
+        <p className='my-1 py-1 text-3xl text-gray-800'>Recent News</p>
+
+
+        <article>
+          <section>
+            {/* <span>Heading </span>
                 <span>date</span> */}
-              </section>
+          </section>
 
-              <section>
-                <section>
-                  <p>thesis paper</p>
-                  <p>authors</p>
-                </section>
+          <section>
+            <section>
+              <p>thesis paper</p>
+              <p>authors</p>
+            </section>
 
-                <section>
-                  <img src="#" />
-                  <p>本文</p>
-                </section>
-              </section>
+            <section>
+              <img src="#" />
+              <p>本文</p>
+            </section>
+          </section>
 
-            </article>
-          </div>
-      
-      </Layout>
-    </>
+        </article>
+      </div>
+
+    </Layout>
   )
 }
 
