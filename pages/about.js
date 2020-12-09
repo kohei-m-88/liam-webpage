02d57649129fffbb30656ea7d02ce5e8620b6b1f
koheiM <matsudakohei88@gmail.com>
2020-12-09 23:08:20 +0900

    erase headerbanner too

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,11 @@
-import HeaderBanner from '../components/headerBanner'
+// import HeaderBanner from '../components/headerBanner'
 // import HeaderMenu from '../components/headerMenu'
 import Layout from '../components/layout'
 
 export default function about() {
   return (
     <Layout>
-      <HeaderBanner banner="B" />
+      {/* <HeaderBanner banner="B" /> */}
       {/* <HeaderMenu aboutMeHover="" aboutMeActive="text-gray-100 bg-gray-900" /> */}
 
     </Layout>
--- pages/research.js
+++ pages/research.js
@@ -1,11 +1,11 @@
 import Layout from '../components/layout'
-import HeaderBanner from '../components/headerBanner'
+// import HeaderBanner from '../components/headerBanner'
 // import HeaderMenu from '../components/headerMenu'
 
 export default function research() {
   return (
     <Layout>
-      <HeaderBanner banner="C" />
+      {/* <HeaderBanner banner="C" /> */}
       {/* <HeaderMenu researchHover="" researchActive="text-gray-100 bg-gray-900" /> */}
 
     </Layout>
