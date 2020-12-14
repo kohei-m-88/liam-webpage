51b12d14f4f678501c723f3df7751d94ca23c8b0
koheiM <matsudakohei88@gmail.com>
2020-12-14 19:52:43 +0900

    modify menu button

--- components/headerMenu.js
+++ components/headerMenu.js
@@ -27,7 +27,7 @@ export default function HeaderMenu(
 
         {menuexpanded
           ? (
-            <button onClick={() => setMenuexpanded(false)} onBlur={() => setMenuexpanded(false)} aria-expanded="true" class={`${styles.openbutton}hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white sm:hidden`}>
+            <button onClick={() => setMenuexpanded(false)} onBlur={() => setMenuexpanded(false)} aria-expanded="true" className={`${styles.openbutton}hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white sm:hidden`}>
               <span class="sr-only">button to close menu</span>
               <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
--- pages/index.js
+++ pages/index.js
@@ -53,7 +53,7 @@ export default function Home({ allPosts }) {
             </div>
           </div>
         ))}
-
+        
       </div>
     </>
   )
