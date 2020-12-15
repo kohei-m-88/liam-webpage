f41975eb9ec306df29bfb1215a836b3ea9c4b071
koheiM <matsudakohei88@gmail.com>
2020-12-15 16:59:20 +0900

    modify thesisPaperLink to be opened by NEW tab

--- components/thesisPaperLink.js
+++ components/thesisPaperLink.js
@@ -1,7 +1,7 @@
 export default function ThesisPaperLink({ tPL }) {
   return (
     <span className="font-bold text-sm sm:text-base md:text-xl lg:text-xl xl:text-2xl"> [
-      <a href={tPL} className="text-blue-600 underline"> Link </a>
+      <a href={tPL} target="_blank" rel="noopener noreferrer" className="text-blue-600 underline"> Link </a>
     ] </span>
   )
 }
