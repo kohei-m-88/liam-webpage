a50b426dc7853b52c1d8d623585892fa6c240a8f
koheiM <matsudakohei88@gmail.com>
2021-03-10 15:53:04 +0900

    change ml

--- components/previewMode.js
+++ components/previewMode.js
@@ -7,7 +7,7 @@ export function PreviewMode() {
         </p>
         <a
           href="/api/exitPreview"
-          className="my-1 py-1 text-green-400 hover:text-cyan font-medium text-sm sm:text-base md:text-xl lg:text-2xl  duration-200 transition-colors underline"
+          className="my-1 ml-3 py-1 text-green-400 hover:text-cyan font-medium text-sm sm:text-base md:text-xl lg:text-2xl  duration-200 transition-colors underline"
         >
           EXIT PREVIEW MODE
         </a>
