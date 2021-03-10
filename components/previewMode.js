8e6f26a67c8aaaa1feeab0f4e30a25a876136ac2
koheiM <matsudakohei88@gmail.com>
2021-03-10 16:56:26 +0900

    add previewmode to research

--- components/previewMode.js
+++ components/previewMode.js
@@ -5,13 +5,14 @@ export function PreviewMode() {
         <p className="my-1 mx-3 py-1 text-red-600 font-medium text-sm sm:text-base md:text-xl lg:text-2xl">
           Preview Mode
         </p>
-        <span>
+        
+        <div>
         <a
           href="/api/exitPreview"
           className="my-1 mx-3 py-1 text-green-400 hover:text-green-700 font-medium text-sm sm:text-base md:text-xl lg:text-2xl  duration-200 transition-colors"
         >
           EXIT PREVIEW MODE
-        </a>←Click</span>
+        </a>←Click</div>
       </div>
     </>
   );
--- pages/research.js
+++ pages/research.js
@@ -2,8 +2,9 @@ import Layout from '../components/layout'
 import HeaderBanner from '../components/headerBanner'
 import HeaderMenu from '../components/headerMenu'
 import { PreviewMode } from "../components/previewMode";
+import { getBioPost } from '../lib/api';
 
-export default function research( preview ) {
+export default function research({preview}) {
   return (
     <Layout title="RESEARCH">
       <HeaderMenu researchHover="" researchActive="text-gray-100 bg-gray-900" />
@@ -12,4 +13,12 @@ export default function research( preview ) {
       <p className="object-center text-2xl p-5 m-5">Coming soon</p>
     </Layout>
   )
+}
+
+// Preview Modeを表示/非表示するために使用
+export async function getStaticProps({preview = false}) {
+  const dummy = await getBioPost();
+  return {
+    props: { preview },
+  };
 }
