a5df26c5b105654fa46fa09a5a83bdea26f18d82
koheiM <matsudakohei88@gmail.com>
2021-03-10 15:43:38 +0900

    add exit preview

--- components/previewMode.js
+++ components/previewMode.js
@@ -1,7 +1,17 @@
 export function PreviewMode() {
   return (
-    <p className="my-1 py-1 text-red-800 font-medium text-sm sm:text-base md:text-xl lg:text-2xl">
-      Preview Mode
-    </p>
-  )
+    <>
+      <div className="flex justify-center">
+        <p className="my-1 py-1 text-red-800 font-medium text-sm sm:text-base md:text-xl lg:text-2xl">
+          Preview Mode
+        </p>
+        <a
+          href="/api/exit-preview"
+          className="underline hover:text-cyan duration-200 transition-colors"
+        >
+          EXIT PREVIEW MODE
+        </a>
+      </div>
+    </>
+  );
 }
