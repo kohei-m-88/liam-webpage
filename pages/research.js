7efbd8f6c20a442078208a8117823d11a7bbff9a
koheiM <matsudakohei88@gmail.com>
2021-03-10 16:40:00 +0900

    add previewmode to research.js

--- pages/research.js
+++ pages/research.js
@@ -1,12 +1,14 @@
 import Layout from '../components/layout'
 import HeaderBanner from '../components/headerBanner'
 import HeaderMenu from '../components/headerMenu'
+import { PreviewMode } from "../components/previewMode";
 
-export default function research() {
+export default function research( preview ) {
   return (
     <Layout title="RESEARCH">
       <HeaderMenu researchHover="" researchActive="text-gray-100 bg-gray-900" />
       <HeaderBanner banner="C" />
+      {preview && <PreviewMode />}
       <p className="object-center text-2xl p-5 m-5">Coming soon</p>
     </Layout>
   )
