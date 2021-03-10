a7fc28556445aee492d410f856c3463418853c00
koheiM <matsudakohei88@gmail.com>
2021-03-10 16:23:02 +0900

    change color

--- components/previewMode.js
+++ components/previewMode.js
@@ -8,7 +8,7 @@ export function PreviewMode() {
         <span>
         <a
           href="/api/exitPreview"
-          className="my-1 mx-3 py-1 text-green-400 hover:text-cyan font-medium text-sm sm:text-base md:text-xl lg:text-2xl  duration-200 transition-colors"
+          className="my-1 mx-3 py-1 text-green-400 hover:text-green-700 font-medium text-sm sm:text-base md:text-xl lg:text-2xl  duration-200 transition-colors"
         >
           EXIT PREVIEW MODE
         </a>←Click</span>
