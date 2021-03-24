a3e8b1d17bc6d40fa363ed5e92d595a80977eff5
koheiM <matsudakohei88@gmail.com>
2021-03-24 17:34:33 +0900

    use <></> at about

--- pages/about.js
+++ pages/about.js
@@ -10,29 +10,30 @@ import { PageSeo } from "../components/seo";
 
 export default function about({ bioPost, preview }) {
   return (
-    <PageSeo
-      p_title="about me"
-      p_url={`${siteMetadata.siteUrl}/about`}
-    />
-    <Layout>
-      <HeaderMenu aboutMeHover="" aboutMeActive="text-gray-100 bg-gray-900" />
-      <HeaderBanner banner="B" />
-      {preview && <PreviewMode />}
+    <>
+      <PageSeo p_title="About Me" p_url={`${siteMetadata.siteUrl}/about`} />
+      <Layout>
+        <HeaderMenu aboutMeHover="" aboutMeActive="text-gray-100 bg-gray-900" />
+        <HeaderBanner banner="B" />
+        {preview && <PreviewMode />}
 
-      <div className="mx-1vw sm:mx-3vw md:mx-5vw my-2vh z-20">
-        <div className="mb-10">
-          <NewsHeading nAHeading={bioPost.heading} nADate={bioPost.date} />
-          <div className="p-3 border-solid border-2 border-gray-800  flow-root">
-            {bioPost.image?.url && <NewsContentPic nAI={bioPost.image?.url} />}
-            <NewsContent nAContent={bioPost.articlecontent?.json} />
+        <div className="mx-1vw sm:mx-3vw md:mx-5vw my-2vh z-20">
+          <div className="mb-10">
+            <NewsHeading nAHeading={bioPost.heading} nADate={bioPost.date} />
+            <div className="p-3 border-solid border-2 border-gray-800  flow-root">
+              {bioPost.image?.url && (
+                <NewsContentPic nAI={bioPost.image?.url} />
+              )}
+              <NewsContent nAContent={bioPost.articlecontent?.json} />
+            </div>
           </div>
         </div>
-      </div>
-    </Layout>
+      </Layout>
+    </>
   );
 }
 
-export async function getStaticProps({preview = false}) {
+export async function getStaticProps({ preview = false }) {
   const bioPost = (await getBioPost(preview)) ?? {};
   return {
     props: { preview, bioPost },
